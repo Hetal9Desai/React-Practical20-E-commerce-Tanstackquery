755d8998aded862b10f288e5d20901d8820b6978
Hetal Desai <hetal.desai@simformsolutions.com>
2025-05-29 15:21:41 +0530

    Add: Add productSlice.ts and ProductForm.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+
+const queryClient = new QueryClient();
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root container missing in index.html");
 
 createRoot(container).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </Provider>
 );
