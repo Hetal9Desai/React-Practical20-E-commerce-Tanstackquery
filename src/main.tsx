c06cfaa5572a831fccdc373f372eeb7fa654f1a5
Hetal Desai <hetal.desai@simformsolutions.com>
2025-05-29 11:49:38 +0530

    Add: Add hooks.ts and store.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import App from "./App";
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Root container missing in index.html");
+
+createRoot(container).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
