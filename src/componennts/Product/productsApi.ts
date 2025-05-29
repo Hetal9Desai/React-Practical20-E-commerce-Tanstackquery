707a421dc7b3e5dbc5333ce3a524bf315145fb54
Hetal9Desai <hetal.desai@simformsolutions.com>
2025-05-29 22:59:02 +0530

    Update productsApi.ts

--- src/componennts/Product/productsApi.ts
+++ src/componennts/Product/productsApi.ts
@@ -1,22 +1,21 @@
 import axios from "axios";
 import type { Product } from "../../types/Product";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
-const BASE = `${API_URL}/products`;
+const URL = "http://localhost:3001/products";
 
 export const fetchProducts = (ownerId: string) =>
-  axios.get<Product[]>(BASE, { params: { ownerId } }).then((res) => res.data);
+  axios.get<Product[]>(URL, { params: { ownerId } }).then((res) => res.data);
 
 export const fetchProduct = (id: string, ownerId: string) =>
   axios
-    .get<Product>(`${BASE}/${id}`, { params: { ownerId } })
+    .get<Product>(`${URL}/${id}`, { params: { ownerId } })
     .then((res) => res.data);
 
 export const createProduct = (p: Omit<Product, "id">) =>
-  axios.post<Product>(BASE, p).then((res) => res.data);
+  axios.post<Product>(URL, p).then((res) => res.data);
 
 export const updateProduct = (id: string, upd: Partial<Product>) =>
-  axios.put<Product>(`${BASE}/${id}`, upd).then((res) => res.data);
+  axios.put<Product>(`${URL}/${id}`, upd).then((res) => res.data);
 
 export const deleteProduct = (id: string | number) =>
-  axios.delete<void>(`${BASE}/${id}`).then((res) => res.data);
+  axios.delete<void>(`${URL}/${id}`).then((res) => res.data);
